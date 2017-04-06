85937d84ad5c56c46e32e6e83d997191cdd0b316
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-06 16:01:28 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,8 +47,7 @@
 
 
         <div class="ui-grid-solo">
-            <div class="ui-block-d">
-                <a href="#" class="ui-btn ui-corner-all ui-shadow">First Column Button</a><br>
+            <div class="ui-block-c">
                 <span>First Column: This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text.</span>
             </div>
         </div>
