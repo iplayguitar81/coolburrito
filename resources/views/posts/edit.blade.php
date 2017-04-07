4f71452fa5fcb2c437be3bbed3503b3f50f6e7d6
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-06 23:07:25 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,6 +2,17 @@
 @section('title', 'Edit '.$post->title)
 @section('content')
 
+    <div data-role="page" data-theme="b" id="loggin2">
+        <div data-role="header" data-tap-toggle="false" data-theme="b">
+            <img class='img-responsive' alt='checkenginefree.com' src='/images/checkenginelogoyay3.png'/>
+            <div data-role="navbar">
+                <ul class="nav-trickery">
+                    <li><a href="https://level3.checkenginefree.com"  data-icon="navigation"><span class="orangose">Map</span></a></li>
+                </ul>
+            </div><!-- /navbar -->
+        </div>
+        <div id="wrappa2">
+
     @can('isAdmin')
 
     <div class="">
@@ -254,6 +265,85 @@
 
         @endcan
     </div>
+
+
+        </div>
+
+
+        <div data-role="footer" style="overflow:hidden;" data-theme="b" data-tap-toggle="false">
+            <span class="credit">checkenginefree.com &copy; 2017</span>
+            <p class="disclaimer">*We make no guarantees that the location information given on this site is accurate.  If you find that there is an inaccurate location listing then please take the time to report it to us using the 'Report this as inaccurate' link.  This will help to ensure that the integrity of our data is to the benefit of yourself and other users.  If you wish to add a new location that folks are able to get their check engine light diagnosed for free then please visit the contact page in order to submit that information to us.  Once it is confirmed we will add it to our databases.  We also make no guarantees that the location listed will offer a free check engine light diagnostic.  This site makes a point of listing the chains that are famous for making such offerings.  Thanks for visiting!  Please tell your friends and family!  :D</p>
+        </div><!-- /footer -->
+    </div>
+
+
+
+
+    {{--<div class="container">--}}
+    {{--<div class="row">--}}
+    {{--<div class="col-md-8 col-md-offset-2">--}}
+    {{--<div class="panel panel-default">--}}
+    {{--<div class="panel-heading">Login</div>--}}
+    {{--<div class="panel-body">--}}
+    {{--<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">--}}
+    {{--{!! csrf_field() !!}--}}
+
+    {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
+    {{--<label class="col-md-4 control-label">E-Mail Address</label>--}}
+
+    {{--<div class="col-md-6">--}}
+    {{--<input type="email" class="form-control" name="email" value="{{ old('email') }}">--}}
+
+    {{--@if ($errors->has('email'))--}}
+    {{--<span class="help-block">--}}
+    {{--<strong>{{ $errors->first('email') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--</div>--}}
+    {{--</div>--}}
+
+    {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
+    {{--<label class="col-md-4 control-label">Password</label>--}}
+
+    {{--<div class="col-md-6">--}}
+    {{--<input type="password" class="form-control" name="password">--}}
+
+    {{--@if ($errors->has('password'))--}}
+    {{--<span class="help-block">--}}
+    {{--<strong>{{ $errors->first('password') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--</div>--}}
+    {{--</div>--}}
+
+    {{--<div class="form-group">--}}
+    {{--<div class="col-md-6 col-md-offset-4">--}}
+    {{--<div class="checkbox">--}}
+    {{--<label>--}}
+    {{--<input type="checkbox" name="remember"> Remember Me--}}
+    {{--</label>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+
+    {{--<div class="form-group">--}}
+    {{--<div class="col-md-6 col-md-offset-4">--}}
+    {{--<button type="submit" class="btn btn-primary">--}}
+    {{--<i class="fa fa-btn fa-sign-in"></i>Login--}}
+    {{--</button>--}}
+
+    {{--<a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</form>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    <script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
+    <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
+
 
 @endsection
 
