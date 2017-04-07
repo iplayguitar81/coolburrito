5ad272b564ecd69ec7a3c473e6edad9c590dcfeb
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-06 21:36:24 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -119,8 +119,8 @@
     </div><!-- /footer -->
 </div>
 
+<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
 <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
-
 @endsection
 
 
