e6f0d6586185eddfa0cc1450a94bd28abb4ca2dd
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-06 20:25:20 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
                         {{$game_date}}
                         <h2>{!! $item->title !!}</h2>
                         <span class="text-body-checkengine">
-                            &nbsp;&nbsp;{{ Str::limit($item->body, 10) }}</span>
+                            &nbsp;&nbsp;{{ limit($item->body, 10) }}</span>
                     </a>
 
 
