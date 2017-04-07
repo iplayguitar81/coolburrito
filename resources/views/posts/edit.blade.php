e9afade0b190f1a3116cf89ea898a5934fe91c4e
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-06 23:13:11 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -341,7 +341,7 @@
     {{--</div>--}}
     {{--</div>--}}
     {{--</div>--}}
-    <script src="{{url('/js/jquery.js')}}"></script>
+
     <script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
     <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 
