58202207a772d48bca4832139c263c7e2f9d2827
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-09 14:05:24 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -145,7 +145,7 @@
 
 @endsection
 
-{{--<script src="{{url('/js/jquery.js')}}"></script>--}}
+<script src="{{url('/js/jquery.js')}}"></script>
 
 
 <script src="{{url('/js/jquery-1.11.3.min.js')}}"></script>
