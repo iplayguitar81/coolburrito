5d2a37a345881718b2c3c16f243c540785a00b91
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-10 23:31:16 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,7 @@
 
                     <div class="form-group">
                         <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
+                            <button data-ajax="false" type="submit" class="btn btn-primary">
                                 <i class="fa fa-btn fa-sign-in"></i>Login
                             </button>
 
