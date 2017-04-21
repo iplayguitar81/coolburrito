d447aa54aa30b0f0207bb6b8542f2e99ff5dfc21
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-20 22:55:09 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                 <ul class="nav-trickery">
 
                  <li>
-                     <div data-role="collapsible" data-collapsed="false" data-collapsed-icon="carat-d">
+                     <div data-role="collapsible" data-collapsed-icon="carat-d">
                              <h4 data-collapsed-icon="carat-d"><span class="orangose">&nbsp;Read Our Blog</span></h4>
                              
                          <ul data-role="listview" data-inset="false">
