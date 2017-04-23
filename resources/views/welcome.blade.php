c7ca4085e1d4b80636fe436cddfdf047372429d6
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-22 22:38:43 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
 
 
                                  <li class="imgListLi" style="width:100%;"><a href="{{ url('posts', $item->id) }}" class="ui-btn ui-icon-carat-r ui-btn-icon-right" data-ajax="false">
-                                         <img class="testImgClass" style="" src="images/{!! 'thmb-'.$item->imgPath !!}">
+                                         <div class="thumbContainer">  <img class="testImgClass" style="" src="images/{!! 'thmb-'.$item->imgPath !!}"></div>
                                          {{$game_date}}
                                          <h2>{!! $item->title !!}</h2>
                                          <p>{!! str_limit($item->body, $limit = 35, $end = '...') !!}</p></a>
