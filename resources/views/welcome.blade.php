08d2891119db98ddcfb627779c83cf27ced2522d
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-24 14:15:39 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -204,25 +204,6 @@
             <br/>
 
 
-            <div id="Glide" class="glide">
-
-                <div class="glide__arrows">
-                    <button class="glide__arrow prev" data-glide-dir="<">prev</button>
-                    <button class="glide__arrow next" data-glide-dir=">">next</button>
-                </div>
-
-                <div class="glide__wrapper">
-                    <ul class="glide__track">
-                        <li class="glide__slide"></li>
-                        <li class="glide__slide"></li>
-                        <li class="glide__slide"></li>
-                    </ul>
-                </div>
-
-                <div class="glide__bullets"></div>
-
-            </div>
-
 
         </div>
         <div data-role="footer" style="overflow:hidden;" data-theme="b" data-tap-toggle="false">
