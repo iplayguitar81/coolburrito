395f14fbd7d76a6945e57948a0b8d1d29b45ee95
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-24 13:57:12 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -401,84 +401,8 @@
     {{--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>--}}
     <!-- bxSlider Javascript file -->
 
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script type="text/javascript" src="js/jquery.slicebox.js"></script>
-    <script type="text/javascript">
-        $(function() {
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/jquery.glide.js"></script>
 
-            var Page = (function() {
-
-                var $navArrows = $( '#nav-arrows' ).hide(),
-                        $navDots = $( '#nav-dots' ).hide(),
-                        $nav = $navDots.children( 'span' ),
-                        $shadow = $( '#shadow' ).hide(),
-                        slicebox = $( '#sb-slider' ).slicebox( {
-                            onReady : function() {
-
-                                $navArrows.show();
-                                $navDots.show();
-                                $shadow.show();
-
-                            },
-                            onBeforeChange : function( pos ) {
-
-                                $nav.removeClass( 'nav-dot-current' );
-                                $nav.eq( pos ).addClass( 'nav-dot-current' );
-
-                            }
-                        } ),
-
-                        init = function() {
-
-                            initEvents();
-
-                        },
-                        initEvents = function() {
-
-                            // add navigation events
-                            $navArrows.children( ':first' ).on( 'click', function() {
-
-                                slicebox.next();
-                                return false;
-
-                            } );
-
-                            $navArrows.children( ':last' ).on( 'click', function() {
-
-                                slicebox.previous();
-                                return false;
-
-                            } );
-
-                            $nav.each( function( i ) {
-
-                                $( this ).on( 'click', function( event ) {
-
-                                    var $dot = $( this );
-
-                                    if( !slicebox.isActive() ) {
-
-                                        $nav.removeClass( 'nav-dot-current' );
-                                        $dot.addClass( 'nav-dot-current' );
-
-                                    }
-
-                                    slicebox.jump( i + 1 );
-                                    return false;
-
-                                } );
-
-                            } );
-
-                        };
-
-                return { init : init };
-
-            })();
-
-            Page.init();
-
-        });
-    </script>
 
 @endsection
