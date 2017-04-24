178596684cf9542d7b01f5f8f38e23067624e4f7
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-24 14:57:25 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
 
 
 
-                      <img class="" src="images/{!! 'thmb-'.$item->imgPath !!}">
+                      <img style="height:100px;width:100px;" class="" src="images/{!! 'thmb-'.$item->imgPath !!}">
                                      <h2>{!! $item->title !!} <p>{{$game_date}}</p>
                                              <p>{!! str_limit($item->body, $limit = 35, $end = '...') !!}</p></h2>
                                      
