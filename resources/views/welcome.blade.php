ed1fe5d6d704952c3b99d2c7d89274ea07108da3
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-24 11:59:50 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -195,11 +195,28 @@
             <br/>
             <br/>
 
-            <ul class="bxslider">
-                <li><img src="/images/apple.png" /></li>
-                <li><img src="/images/apple.png" /></li>
-                <li><img src="/images/apple.png" /></li>
-                <li><img src="/images/apple.png" /></li>
+            <ul id="sb-slider" class="sb-slider">
+
+                <li>
+                    <a href="#" target="_blank">
+                        <img src="images/1.jpg" alt="image1"/>
+                    </a>
+                    <div class="sb-description">
+                        <h3>Creative Lifesaver</h3>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="images/2.jpg" alt="image2"/>
+                    <div class="sb-description">
+                        <h3>...</h3>
+                    </div>
+                </li>
+
+                <li><!-- ... --></li>
+
+                <!-- ... -->
+
             </ul>
 
         </div>
@@ -390,8 +407,64 @@
     <!-- bxSlider CSS file -->
 
     <script>
+        $.Slicebox.defaults = {
+            // (v)ertical, (h)orizontal or (r)andom
+            orientation : 'v',
+
+            // perspective value
+            perspective : 1200,
+
+            // number of slices / cuboids
+            // needs to be an odd number 15 => number > 0 (if you want the limit higher,
+            // change the _validate function).
+            cuboidsCount : 5,
+
+            // if true then the number of slices / cuboids is going to be random (cuboidsCount is overwitten)
+            cuboidsRandom : false,
+
+            // the range of possible number of cuboids if cuboidsRandom is true
+            // it is strongly recommended that you do not set a very large number :)
+            maxCuboidsCount : 5,
+
+            // each cuboid will move x pixels left / top (depending on orientation).
+            // The middle cuboid doesn't move. the middle cuboid's neighbors will
+            // move disperseFactor pixels
+            disperseFactor : 0,
+
+            // color of the hidden sides
+            colorHiddenSides : '#222',
+
+            // the animation will start from left to right. The left most
+            // cuboid will be the first one to rotate
+            // this is the interval between each rotation in ms
+            sequentialFactor : 150,
+
+            // animation speed
+            // this is the speed that takes "1" cuboid to rotate
+            speed : 600,
+
+            // transition easing
+            easing : 'ease',
+
+            // if true the slicebox will start the animation automatically
+            autoplay : false,
+
+            // time (ms) between each rotation, if autoplay is true
+            interval: 3000,
+
+            // the fallback will just fade out / fade in the items
+            // this is the time fr the fade effect
+            fallbackFadeSpeed : 300,
+
+            // callbacks
+            onBeforeChange : function( position ) { return false; },
+            onAfterChange : function( position ) { return false; }
+        };
+    </script>
+
+    <script>
     $(document).ready(function(){
-        $('.bxslider').bxSlider();
+        $('#sb-slider').slicebox();
     });
 
 </script>
