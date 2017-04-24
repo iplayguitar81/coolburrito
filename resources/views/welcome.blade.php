513f79c8576acc258399efb6d0987ec274f6d10a
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-23 18:34:46 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,11 +97,7 @@
 
 
 
-                <div data-role="page" data-theme="b" id="demo-page" class="my-page" data-url="demo-page">
-                    <div data-role="header">
-                        <h1>Listview Responsive Grid</h1>
-                        <a href="./" data-shadow="false" data-iconshadow="false" data-icon="carat-l" data-iconpos="notext" data-rel="back" data-ajax="false">Back</a>
-                    </div><!-- /header -->
+
                     <div role="main" class="ui-content">
                         <ul data-role="listview" data-inset="true">
                             <li><a href="#">
