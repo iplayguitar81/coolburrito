707778dd00b2e98b05530fe93b941b7a52325697
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-24 12:04:49 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -403,7 +403,7 @@
     <!-- jQuery library (served from Google) -->
     {{--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>--}}
     <!-- bxSlider Javascript file -->
-    <script src="/js/jquery.bxslider.min.js"></script>
+    <script src="/js/jquery.slicebox.js"></script>
     <!-- bxSlider CSS file -->
 
     <script>
