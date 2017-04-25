dfd787617e31ed59f0bc352005f4fb33a4b6e870
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-25 14:30:51 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,23 +56,9 @@
 
 
 
-                             <div data-role="collapsible" data-theme="b" data-inset="true">
-                                 <h3>
-                                     <div class="ui-grid-a">
-                                         <div class="ui-block-a">Customer:</div>
-                                         <div class="un-block-b">Plumbing</div>
-                                     </div>
-                                 </h3>
-                                 <ul data-role="listview">
-                                     <li><div class="ui-grid-a">
-                                             <div class="ui-block-a">Phone:</div>
-                                             <div class="ui-block-b">1-800-MUS-ROOM</div>
-                                         </div></li>
-                                     <li><div class="ui-grid-a">
-                                             <div class="ui-block-a">More data:</div>
-                                             <div class="ui-block-b">blah blah blah</div>
-                                         </div></li>
-                                 </ul>
+                             <div class="ui-grid-a">
+                                 <div class="ui-block-a">Customer:</div>
+                                 <div class="un-block-b">Plumbing</div>
                              </div>
 
 
