c66a41c6f090e6097b71255f159753cc7fa0c1a6
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-25 15:24:58 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
 
 
                                  <li style="width:100%;"><a href="{{ url('posts', $item->id) }}" class="ui-btn ui-icon-carat-r ui-btn-icon-right" data-ajax="false">
-                                         <img src="images/{!! 'thmb-'.$item->imgPath !!}" title="sample"/>
+                                         <img style="width:150px;" src="images/{!! 'thmb-'.$item->imgPath !!}" title="sample"/>
                                          <h3>{!! $item->title !!} {{$game_date}}</h3>
                                          <p>{!! str_limit($item->body, $limit = 35, $end = '...') !!}</p>
                                      </a></li>
