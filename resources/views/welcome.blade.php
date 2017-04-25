686ae6099e88fce72815266e2cb8e19f43e01ab2
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-25 15:07:27 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -223,23 +223,7 @@
             <br/>
 
 
-            <!-- Swiper -->
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
-                    <div class="swiper-slide">Slide 10</div>
-                </div>
-                <!-- Add Pagination -->
-                <div class="swiper-pagination"></div>
-            </div>
+
 
 
 
@@ -417,16 +401,7 @@
     <script type="text/javascript" src="/js/jquery.validate.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 
-    <script src="/js/swiper.min.js"></script>
 
-    <script>
-        var swiper = new Swiper('.swiper-container', {
-            pagination: '.swiper-pagination',
-            slidesPerView: 3,
-            paginationClickable: true,
-            spaceBetween: 30
-        });
-    </script>
 
     <script type="text/javascript" src="https://maps-api-ssl.google.com/maps/api/js?key=AIzaSyBS5pkvW9RGZEcNC04MWLQoA3DySvAFLmg&amp;sensor=true&amp;v=3&amp;libraries=geometry" ></script>
     <script type="text/javascript" async>
