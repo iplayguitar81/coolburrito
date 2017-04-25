c3152037966efad6386e289a1ad5b3682a7f40ed
iplayguitar81 <iplayguitar81@yahoo.com>
2017-04-25 14:46:30 -0700

    get win 10 laravel started yeahhhhhh

--- public/images/layout.blade.php
+++ public/images/layout.blade.php
@@ -293,20 +293,9 @@
     }
 
 
-    <!-- Demo styles -->
-    <style>
-     html, body {
-         position: relative;
-         height: 100%;
-     }
-    body {
-        background: #eee;
-        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-        font-size: 14px;
-        color:#000;
-        margin: 0;
-        padding: 0;
-    }
+    /* Swiper CSS*/
+
+
     .swiper-container {
         width: 100%;
         height: 100%;
@@ -330,8 +319,6 @@
         -webkit-align-items: center;
         align-items: center;
     }
-</style>
-
 
 </style>
 
