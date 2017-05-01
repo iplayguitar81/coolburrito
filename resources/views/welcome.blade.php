bf6a185275fa3ae41847b35ec0b7ea6fdaa07f55
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-01 11:29:34 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
                                                  {{$game_date}}<br/>
                                                  <br/>
                                                  <div class="thumb-size" > <img style="max-width:100%  !important; height:auto; display:inline;" src="images/{!! 'thmb-'.$item->imgPath !!}" title="sample"/></div>
-                                                 <h3><span class="orangose_blog-mini">{!! $item->title !!}</span> </h3>
+                                                 <h3><span style="color: black;" class="orangose_blog-mini">{!! $item->title !!}</span> </h3>
                                                  <p>{!! str_limit($item->body, $limit = 35, $end = '...') !!}</p>
                                              </a></li>
 
