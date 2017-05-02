268fa013f713fecee1e9964098d3854f200370f5
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-01 20:12:45 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
 
                                                              {{  $item->title }}
 
-                                                         @endforeach
+                                                         @endif
 
 
                                                      </span> </h3>
