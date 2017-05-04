c42e5d289c91332ab6cabf954caffc35cd2cead1
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-03 22:35:28 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -91,7 +91,7 @@
                         {{--*/ $thumb_path= substr($image->file_path, 7);/*--}}
                         <li class="owl-trick">
                             <a href="{{url($image->file_path)}}"  data-size="{{$dimensions}}" data-title="{{$image->caption}}">
-                                <img class="img-responsive" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
+                                <img class="img-responsive show-main-img" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
                     @endforeach
 
                 </ul>
@@ -240,6 +240,9 @@
 
     .show-article {
         background-color:#323333;
+    }
+    .show-main-img {
+        border: 1px solid orange;
     }
 
     .contact_header,.contact_header2{font-variant:small-caps;
