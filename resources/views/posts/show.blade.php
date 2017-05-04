19d680025e131787320af8e3985cdd90f3531570
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-03 19:36:39 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -239,6 +239,11 @@
 
 
     .contact_header,.contact_header2{font-size: 2em !important;text-align:center;color:#ff9000;text-shadow:2px 2px #bb2103}
+
+    .subheader-main {
+        text-align:center;
+
+    }
 </style>
 
 
