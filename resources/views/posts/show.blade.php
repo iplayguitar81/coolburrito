189434a6a5452b36990b76e00ab1f4ff190a12d6
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-10 02:23:01 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,6 @@
 {{--@extends('layout')--}}
 
 
-
 <div id="fb-root"></div>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -27,7 +26,7 @@
             <img class='img-responsive' alt='checkenginefree.com' src='/images/checkenginelogoyay3.png'/>
             <div data-role="navbar">
                 <ul class="nav-trickery">
-                    <li><a href="https://level3.checkenginefree.com"  data-icon="navigation" data-ajax="false"><span class="orangose">Map</span></a></li>
+                    <li><a href="https://level3.checkenginefree.com"  data-icon="navigation" data-ajax="false"><span class="orangose">Back to Map</span></a></li>
                 </ul>
             </div><!-- /navbar -->
         </div>
@@ -39,7 +38,7 @@
             <div class="row">
     <div class="">
 
-    <article class="center-block">
+    <article class="show-article center-block">
         <h2 class="contact_header">{{$post->title}}</h2>
         <p class="subheader-main Bebas">{{ $post->subHead}}</p>
 
@@ -66,14 +65,15 @@
             <li><a href="http://www.reddit.com/submit?url=https%3A%2F%2Flevel3.checkenginefree.com&title=" target="_blank" title="Submit to Reddit" onclick="window.open('http://www.reddit.com/submit?url=' + encodeURIComponent(document.URL) + '&title=' +  encodeURIComponent(document.title)); return false;"><img alt="Submit to Reddit" src="{{url('images/Reddit.png')}}"></a></li>
         </ul>
         <br/>
-        <p class="uk-article-lead"><img style="text-align:center;margin-left:auto;margin-right:auto;display:block;"class="img-responsive center-block" src='{{"../../images/". $post->imgPath}}'></p>
+        <p class="uk-article-lead"><img style="text-align:center;margin-left:auto;margin-right:auto;display:block;"class="show-main-img center-block" src='{{"../../images/". $post->imgPath}}'></p>
 
         <br/>
         <div class="center-block text-center">
        <div class="article-texterson2"> {!! ($post->body) !!} </div>
             @if(($post->images->count() > 0 ))
             <div class="container">
-                <h2 class='Bebas'>article gallery</h2>
+
+                <div class="article-gallery-header-img"><img class="" src="/images/article-gallery.png" alt="article gallery" /></div>
                     <br/>
                 {{--<div class="customNavigation">--}}
                 {{--<a class="btn prev btn-danger">Previous</a>--}}
@@ -90,7 +90,7 @@
 
                         {{--*/ $thumb_path= substr($image->file_path, 7);/*--}}
                         <li class="owl-trick">
-                            <a href="{{url($image->file_path)}}"  data-size="{{$dimensions}}" data-title="{{$image->caption}}">
+                            <a href="{{url($image->file_path)}}" data-ajax="false"  data-size="{{$dimensions}}" data-title="{{$image->caption}}">
                                 <img class="img-responsive" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
                     @endforeach
 
@@ -106,32 +106,43 @@
 
         <br/>
     </div>
+                <hr>
 
-    <div class="">
+    <div class="facebook-section-comments">
 
-        <h2 class="text-center Bebas" >leave a facebook comment!</h2>
+        <h2 class="facebook-comment-header text-center Bebas" >leave a facebook comment!</h2>
         <div class="fb-comments center-block" data-href="https://level3.checkenginefree.com/posts/{{$post->id}}/{{str_slug($post->title)}}" data-numposts="10"></div>
 
-        <br/>
+
+
+    </div>
+                <hr>
+
+                <br/>
+                <br/>
 
     <a href="{{url('/news')}}" data-ajax="false">
 
-        <button type="submit" class="btn btn-primary center-block btn-md" >Back to All Posts</button>
+        <button type="submit" data-theme="a" class="btn btn-primary center-block btn-md"> <span class="orangose3">Back to All Posts</span> </button>
     </a>
    &nbsp;
     <a href="{{url('/')}}" data-ajax="false">
 
-        <button type="submit" class="btn btn-danger center-block btn-md" >Back Home</button>
+        <button type="submit" data-theme="a" class="btn btn-danger center-block btn-md"> <span class="orangose3">Back to Map</span> </button>
     </a>
 
 
-</div>
+        <br/>
+        <br/>
+
+
 
     </div>
 
 
             </div>
         </div>
+
 
 
         <div data-role="footer" style="overflow:hidden;" data-theme="b" data-tap-toggle="false">
@@ -152,69 +163,50 @@
     @import "https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.css";
     @import "{{url('/css/default-skin.css')}}";
     @import "{{url('/css/lightslider.css')}}";
-    {{--getting this part right........ among files to remove after figuring out right gallery sitch:--}}
-     {{--@import "{{url('/css/slick.css')}}";--}}
-    {{--@import "{{url('/css/slick-theme.css')}}";--}}
-    {{--@import "{{url('/css/slick-theme.css')}}";--}}
+
     @import "{{url('/css/owl.carousel.css')}}";
     @import "{{url('/css/owl.theme.css')}}";
     @import "{{url('/css/photoswipe.css')}}";
     @import "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css";
-    @import "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css";
     @import "//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css";
     @import "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/fonts/glyphicons-halflings-regular.woff";
-    #gallery-images img {
-        width: 240px;
-        height: 160px;
-        border: 2px solid black;
-        margin-bottom: 10px;
+
+    .ui-mobile a img, .ui-mobile fieldset {
+         border-width: 1px !important;
     }
-    #gallery-images ul {
-        margin: 0;
-    }
-    #gallery-images li {
-        margin: 0;
-        padding: 0;
-        list-style: none;
-        float: left;
-        padding-right: 10px;
-    }
-    #owl-demo .item{
-    margin: 3px;
-    width: 80%;
-    margin-left:10%;
-    margin-right:10%;
-    }
-    #owl-demo .item img{
-    display: block;
-    width: 100%;
-    height: auto;
-    }
-    .customNavigation{
-    text-align: center;
-    }
-    .customNavigation a{
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    }
-    .owl-carousel li {
-        list-style:none;
-        margin-right: .3em;
-    }
-    .owl-carousel li img {
-        border-radius:.5em;
-        transition:transform .15s ease-out;
-    }
-    .owl-carousel li img:hover {
-        transform:scale(.98, .98);
-    }
-    .owl-carousel li img:active {
-        transform:scale(.96, .96);
-    }
+
+    /*#gallery-images img {*/
+        /*width: 240px;*/
+        /*height: 160px;*/
+        /*border: 2px solid black;*/
+        /*margin-bottom: 10px;*/
+    /*}*/
+    /*#gallery-images ul {*/
+        /*margin: 0;*/
+    /*}*/
+    /*#gallery-images li {*/
+        /*margin: 0;*/
+        /*padding: 0;*/
+        /*list-style: none;*/
+        /*float: left;*/
+        /*padding-right: 10px;*/
+    /*}*/
+
+    /*.owl-carousel li {*/
+        /*list-style:none;*/
+        /*margin-right: .3em;*/
+    /*}*/
+    /*.owl-carousel li img {*/
+        /*border-radius:.5em;*/
+        /*transition:transform .15s ease-out;*/
+        /*border: 1px orange solid;*/
+    /*}*/
+    /*.owl-carousel li img:hover {*/
+        /*transform:scale(.98, .98);*/
+    /*}*/
+    /*.owl-carousel li img:active {*/
+        /*transform:scale(.96, .96);*/
+    /*}*/
 
     ul.share-buttons{
         list-style: none;
@@ -236,6 +228,78 @@
         width: 1px;
         overflow: hidden;
     }
+
+    #about_us_words{
+        background-color:#323333;
+    }
+#wrappa2{
+    background-color:#323333;
+}
+
+    .article-texterson2 {
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .show-main-img {
+        border: 1px solid #ff9000;
+        max-width:100%  !important;
+        height:auto;
+        display:inline;
+    }
+
+
+    .contact_header,.contact_header2{font-variant:small-caps;
+        font-size: 2em !important;
+        text-align:center;
+        color:#ff9000;
+        text-shadow: none !important;}
+
+    .subheader-main {
+        text-align:center;
+
+
+    }
+   .article-gallery-header-img {
+
+        text-align:center;
+    }
+
+
+    .facebook-comment-header {
+    color: #3c55a2;
+        text-align:center;
+    }
+
+    .facebook-section-comments {
+        background-color: #d3d8da;
+
+    }
+
+
+    /*.owl-theme .owl-controls .owl-buttons div {*/
+        /*color: #FFF;*/
+        /*display: inline-block;*/
+        /*zoom: 1;*/
+        /*margin: 5px;*/
+        /*padding: 3px 10px;*/
+        /*font-size: 12px;*/
+        /*-webkit-border-radius: 30px;*/
+        /*-moz-border-radius: 30px;*/
+        /*border-radius: 30px;*/
+        /*background: #ff9000;*/
+        /*!*filter: Alpha(Opacity=50);*!*/
+         /*opacity: 1.0;*/
+    /*}*/
+
+    .owl-stuff{
+        /*width:100%;*/
+        /*height:auto !important;*/
+
+
+    }
+
 </style>
 
 
@@ -417,7 +481,7 @@
 <script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
 <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 
-{{--<script src="{{url('/js/jquery-1.11.3.min.js')}}"></script>--}}
+<script src="{{url('/js/jquery-1.11.3.min.js')}}"></script>
 <script src="{{url('/js/star-rating.js')}}"></script>
 <script src="{{url('/js/owl.carousel.js')}}"></script>
 {{--<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>--}}
