33f340ac0f342c3dcb270a324c3e991663a04226
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-13 16:50:08 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -81,6 +81,7 @@
                 {{--<a class="btn prev btn-danger">Previous</a>--}}
                 {{--<a class="btn next btn-danger">Next</a>--}}
                 {{--</div>--}}
+                <div class="woodsy-pollute">
                 <ul class="owl-carousel">
                     @foreach($post->images as $image)
 
@@ -97,7 +98,7 @@
                     @endforeach
 
                 </ul>
-
+                </div>
             </div>
                 @endif
                     </div>
@@ -314,6 +315,11 @@
     img.logo-show {
 
         background-color: #1d1d1d !important;
+    }
+
+    .woodsy-pollute{
+        margin-left:auto;
+        margin-right:auto;
     }
 
 </style>
