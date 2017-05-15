4fdafe96c1442a43d8cbad8fd3200fc0c3fb45e9
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-14 22:17:13 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -295,7 +295,7 @@
 
     .owl-theme .owl-controls .owl-buttons div {
         color: #FFF;
-        display: inline-block;
+        display: inline;
         zoom: 1;
         margin: 5px;
         padding: 3px 10px;
