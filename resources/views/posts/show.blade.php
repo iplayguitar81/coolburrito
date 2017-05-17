4b670a77976a8ad5d0b7937c2b232d3a161abf08
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-16 23:56:58 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -39,10 +39,8 @@
     <div class="">
 
     <article class="show-article center-block">
-        <h2 style="text-align:center;"><span class="contact_header">{{$post->title}}</span><br/>
-            <span class="subheader-main Bebas">{{ $post->subHead}}</span>
-        </h2>
-
+        <h2 class="contact_header">{{$post->title}}</h2>
+        <p class="subheader-main Bebas">{{ $post->subHead}}</p>
 
         <p class="uk-article-meta" style="text-align:center;">
             Written by <?
@@ -81,8 +79,7 @@
                 {{--<a class="btn prev btn-danger">Previous</a>--}}
                 {{--<a class="btn next btn-danger">Next</a>--}}
                 {{--</div>--}}
-                <div class="woodsy-pollute">
-                <ul class="owl-carousel" style="display:inline !important;">
+                <ul class="owl-carousel">
                     @foreach($post->images as $image)
 
                         {{--*/ @ $pathy =$image->file_path  /*--}}
@@ -94,11 +91,11 @@
                         {{--*/ $thumb_path= substr($image->file_path, 7);/*--}}
                         <li class="owl-trick">
                             <a href="{{url($image->file_path)}}" data-ajax="false"  data-size="{{$dimensions}}" data-title="{{$image->caption}}">
-                                <img class="owl-stuff img-responsive" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
+                                <img class="img-responsive" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
                     @endforeach
 
                 </ul>
-                </div>
+
             </div>
                 @endif
                     </div>
@@ -177,7 +174,7 @@
 
     @font-face {
         font-family: 'CheckLogo';
-        src: url("https://dl.dropboxusercontent.com/s/b0vl8untp94wys3/ARB.ttf"); !important
+        src: url("https://www.dropbox.com/s/b0vl8untp94wys3/ARB.ttf"); !important
     }
 
     .ui-mobile a img, .ui-mobile fieldset {
@@ -199,13 +196,6 @@
         list-style: none;
         float: left;
         padding-right: 10px;
-    }
-
-
-
-
-    .owl-wrapper {
-        margin: 0 auto;
     }
 
     .owl-carousel li {
@@ -267,12 +257,11 @@
 
 
     .contact_header,.contact_header2{font-variant:small-caps;
-        font-size: 3em !important;
+        font-size: 2em !important;
         text-align:center;
-        color:#fcb704;
+        color:#ff9000;
+        text-shadow: none !important;
         font-family: CheckLogo, Tahoma, Arial, "Trebuchet MS";
-        text-shadow: 6px 1px 10px #bb2103 !important;
-        line-height: .9em;
     }
 
     .subheader-main {
@@ -322,13 +311,7 @@
     img.logo-show {
 
         background-color: #1d1d1d !important;
-        height:auto !important;
     }
-
-    /*.woodsy-pollute{*/
-        /*margin-left:auto;*/
-        /*margin-right:auto;*/
-    /*}*/
 
 </style>
 
@@ -337,7 +320,174 @@
 
 
 
+<script>
 
+    $(function(){
+
+        // Drawing the HTML for PhotoSwipe
+        function buildPswdHtml(){
+            $("body").append([
+                '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">',
+                '  <div class="pswp__bg"></div>',
+                '  <div class="pswp__scroll-wrap">',
+                '    <div class="pswp__container">',
+                '      <div class="pswp__item"></div>',
+                '      <div class="pswp__item"></div>',
+                '      <div class="pswp__item"></div>',
+                '    </div>',
+                '    <div class="pswp__ui pswp__ui--hidden">',
+                '      <div class="pswp__top-bar">',
+                '          <div class="pswp__counter"></div>',
+                '          <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>',
+                '          <button class="pswp__button pswp__button--share" title="Share"></button>',
+                '          <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>',
+                '          <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>',
+                '          <div class="pswp__preloader">',
+                '            <div class="pswp__preloader__icn">',
+                '              <div class="pswp__preloader__cut">',
+                '                <div class="pswp__preloader__donut"></div>',
+                '              </div>',
+                '            </div>',
+                '          </div>',
+                '      </div>',
+                '      <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">',
+                '        <div class="pswp__share-tooltip"></div> ',
+                '      </div>',
+                '      <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>',
+                '      <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>',
+                '      <div class="pswp__caption">',
+                '        <div class="pswp__caption__center"></div>',
+                '      </div>',
+                '    </div>',
+                '  </div>',
+                '</div>'
+            ].join(""));
+        }
+
+
+        // From the gallery, get the items for PhotoSwipe
+        function getGalleryItems($gallery){
+            var items = [];
+
+            $gallery.find("a").each(function(){
+                var $anchor = $(this),
+                        size = $anchor.attr("data-size").split("x"),
+                        title = $anchor.attr("data-title"),
+                        item = {
+                            el: $anchor.get(0),
+                            src: $anchor.attr("href"),
+                            w: parseInt(size[0]),
+                            h: parseInt(size[1])
+                        };
+
+                // caption
+                if( title ) item.title = title;
+
+                items.push(item);
+            });
+
+            return items;
+        }
+
+
+        //Opening the PhotoSwipe
+        function openGallery($gallery, index, items, pswpOptions){
+            var $pswp = $(".pswp"),
+                    owl = $gallery.data("owlCarousel"),
+                    gallery;
+
+            //Set an option value
+            var options = $.extend(true, {
+                // Image number to open
+                index: index,
+
+                //Zoom setting at the time of image click
+                getThumbBoundsFn: function(index){
+                    var $thumbnail = $(items[index].el).find("img"),
+                            offset = $thumbnail.offset();
+                    return {
+                        x: offset.left,
+                        y: offset.top,
+                        w: $thumbnail.outerWidth()
+                    };
+                }
+            }, pswpOptions);
+
+            //Display the PhotoSwipe
+            gallery = new PhotoSwipe($pswp.get(0), PhotoSwipeUI_Default, items, options);
+            gallery.init();
+
+            // In accordance with the switching of PhotoSwipe slide , OwlCarousel also adjusts position
+            gallery.listen("beforeChange", function(x){
+                owl.goTo(this.getCurrentIndex());
+            });
+
+            gallery.listen("close", function(){
+                this.currItem.initialLayout = options.getThumbBoundsFn(this.getCurrentIndex());
+            });
+        }
+
+
+        // Initialization
+        function initializeGallery($elem, owlOptions, pswpOptions){
+
+            //If the DOM for PhotoSwipe does not exist , a new drawing
+            if( $(".pswp").size() === 0 ){
+                buildPswdHtml();
+            }
+
+            // Scan to accommodate a plurality of gallery
+            $elem.each(function(i){
+                var $gallery = $(this),
+                        uid = i + 1,
+                        items = getGalleryItems($gallery),
+                        options = $.extend(true, {}, pswpOptions);
+
+                // Initialization of OwlCarousel
+                $gallery.owlCarousel(owlOptions);
+
+                //Assign a unique ID to each gallery
+                options.galleryUID = uid;
+                $gallery.attr("data-pswp-uid", uid);
+
+                // With the click of each item , start PhotoSwipe
+                $gallery.find(".owl-item").on("click", function(e){
+                    if( !$(e.target).is("img") ) return;
+
+                    //items pass a copy because it is rewritten to PhotoSwipe.init ()
+                    openGallery($gallery, $(this).index(), items.concat(), options);
+                    return false;
+                });
+            });
+        }
+
+
+        // In the sample to perform the processing for the `.owl-carousel`
+        var owlOptions = {
+            //what to mess with if you want to change the amount of slides on the page no matter what as default 3 will show up...
+//                    itemsCustom: [[0, 3]],
+                    items: 3,
+                    responsiveRefreshRate: 0,
+                    navigation: true,
+//                    pagination: true,
+//                    paginationNumbers: true,
+                    scrollPerPage: false,
+                    dots: true,
+                    dotsEach: true
+                },
+                pswpOptions = {
+                    bgOpacity: 0.9,
+                    history: false,
+                    shareEl: true
+                };
+
+        initializeGallery($(".owl-carousel"), owlOptions, pswpOptions);
+
+    });
+
+
+
+</script>
 
 
 
