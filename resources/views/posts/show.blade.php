17e5cdf0ca86dbba24770722039524c49c19b959
iplayguitar81 <iplayguitar81@yahoo.com>
2017-05-16 23:46:10 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,6 @@
 {{--@extends('layout')--}}
 
 
-
 <div id="fb-root"></div>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -24,7 +23,7 @@
 
     <div data-role="page" data-theme="b" id="loggin2">
         <div data-role="header" data-tap-toggle="false" data-theme="b">
-            <img class='img-responsive' alt='checkenginefree.com' src='/images/checkenginelogoyay3.png'/>
+            <img class='logo-show img-responsive' alt='checkenginefree.com' src='/images/checkenginelogoyay3.png'/>
             <div data-role="navbar">
                 <ul class="nav-trickery">
                     <li><a href="https://level3.checkenginefree.com"  data-icon="navigation" data-ajax="false"><span class="orangose">Back to Map</span></a></li>
@@ -39,9 +38,11 @@
             <div class="row">
     <div class="">
 
-    <article class="center-block">
-        <h2 class="contact_header">{{$post->title}}</h2>
-        <p class="subheader-main Bebas">{{ $post->subHead}}</p>
+    <article class="show-article center-block">
+        <h2 style="text-align:center;"><span class="contact_header">{{$post->title}}</span><br/>
+            <span class="subheader-main Bebas">{{ $post->subHead}}</span>
+        </h2>
+
 
         <p class="uk-article-meta" style="text-align:center;">
             Written by <?
@@ -66,20 +67,22 @@
             <li><a href="http://www.reddit.com/submit?url=https%3A%2F%2Flevel3.checkenginefree.com&title=" target="_blank" title="Submit to Reddit" onclick="window.open('http://www.reddit.com/submit?url=' + encodeURIComponent(document.URL) + '&title=' +  encodeURIComponent(document.title)); return false;"><img alt="Submit to Reddit" src="{{url('images/Reddit.png')}}"></a></li>
         </ul>
         <br/>
-        <p class="uk-article-lead"><img style="text-align:center;margin-left:auto;margin-right:auto;display:block;"class="img-responsive center-block" src='{{"../../images/". $post->imgPath}}'></p>
+        <p class="uk-article-lead"><img style="text-align:center;margin-left:auto;margin-right:auto;display:block;"class="show-main-img center-block" src='{{"../../images/". $post->imgPath}}'></p>
 
         <br/>
         <div class="center-block text-center">
        <div class="article-texterson2"> {!! ($post->body) !!} </div>
             @if(($post->images->count() > 0 ))
             <div class="container">
-                <h2 class='Bebas'>article gallery</h2>
+
+                <div class="article-gallery-header-img"><img class="" src="/images/article-gallery.png" alt="article gallery" /></div>
                     <br/>
                 {{--<div class="customNavigation">--}}
                 {{--<a class="btn prev btn-danger">Previous</a>--}}
                 {{--<a class="btn next btn-danger">Next</a>--}}
                 {{--</div>--}}
-                <ul class="owl-carousel">
+                <div class="woodsy-pollute">
+                <ul class="owl-carousel test-owl">
                     @foreach($post->images as $image)
 
                         {{--*/ @ $pathy =$image->file_path  /*--}}
@@ -90,12 +93,12 @@
 
                         {{--*/ $thumb_path= substr($image->file_path, 7);/*--}}
                         <li class="owl-trick">
-                            <a href="{{url($image->file_path)}}"  data-size="{{$dimensions}}" data-title="{{$image->caption}}">
-                                <img class="img-responsive" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
+                            <a href="{{url($image->file_path)}}" data-ajax="false"  data-size="{{$dimensions}}" data-title="{{$image->caption}}">
+                                <img class="owl-stuff img-responsive" src="{{url('images/thmb-'.$thumb_path)}}" alt="1"></a></li>
                     @endforeach
 
                 </ul>
-
+                </div>
             </div>
                 @endif
                     </div>
@@ -106,32 +109,43 @@
 
         <br/>
     </div>
+                <hr>
 
-    <div class="">
+    <div class="facebook-section-comments">
 
-        <h2 class="text-center Bebas" >leave a facebook comment!</h2>
+        <h2 class="facebook-comment-header text-center Bebas" >leave a facebook comment!</h2>
         <div class="fb-comments center-block" data-href="https://level3.checkenginefree.com/posts/{{$post->id}}/{{str_slug($post->title)}}" data-numposts="10"></div>
 
-        <br/>
+
+
+    </div>
+                <hr>
+
+                <br/>
+                <br/>
 
     <a href="{{url('/news')}}" data-ajax="false">
 
-        <button type="submit" class="btn btn-primary center-block btn-md" >Back to All Posts</button>
+        <button type="submit" data-theme="a" class="btn btn-primary center-block btn-md"> <span class="orangose3">Back to All Posts</span> </button>
     </a>
    &nbsp;
     <a href="{{url('/')}}" data-ajax="false">
 
-        <button type="submit" class="btn btn-danger center-block btn-md" >Back Home</button>
+        <button type="submit" data-theme="a" class="btn btn-danger center-block btn-md"> <span class="orangose3">Back to Map</span> </button>
     </a>
 
 
-</div>
+        <br/>
+        <br/>
+
+
 
     </div>
 
 
             </div>
         </div>
+
 
 
         <div data-role="footer" style="overflow:hidden;" data-theme="b" data-tap-toggle="false">
@@ -152,17 +166,24 @@
     @import "https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.css";
     @import "{{url('/css/default-skin.css')}}";
     @import "{{url('/css/lightslider.css')}}";
-    {{--getting this part right........ among files to remove after figuring out right gallery sitch:--}}
-     {{--@import "{{url('/css/slick.css')}}";--}}
-    {{--@import "{{url('/css/slick-theme.css')}}";--}}
-    {{--@import "{{url('/css/slick-theme.css')}}";--}}
+
     @import "{{url('/css/owl.carousel.css')}}";
     @import "{{url('/css/owl.theme.css')}}";
     @import "{{url('/css/photoswipe.css')}}";
     @import "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css";
-    @import "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css";
     @import "//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css";
     @import "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/fonts/glyphicons-halflings-regular.woff";
+
+
+    @font-face {
+        font-family: 'CheckLogo';
+        src: url("https://dl.dropboxusercontent.com/s/b0vl8untp94wys3/ARB.ttf"); !important
+    }
+
+    .ui-mobile a img, .ui-mobile fieldset {
+         border-width: 1px !important;
+    }
+
     #gallery-images img {
         width: 240px;
         height: 160px;
@@ -179,28 +200,10 @@
         float: left;
         padding-right: 10px;
     }
-    /*#owl-demo .item{*/
-    /*margin: 3px;*/
-    /*width: 80%;*/
-    /*margin-left:10%;*/
-    /*margin-right:10%;*/
-    /*}*/
-    /*#owl-demo .item img{*/
-    /*display: block;*/
-    /*width: 100%;*/
-    /*!*height: auto;*!*/
-    /*}*/
-    /*.customNavigation{*/
-    /*text-align: center;*/
-    /*}*/
-    /*.customNavigation a{*/
-    /*-webkit-user-select: none;*/
-    /*-khtml-user-select: none;*/
-    /*-moz-user-select: none;*/
-    /*-ms-user-select: none;*/
-    /*user-select: none;*/
-    /*-webkit-tap-highlight-color: rgba(0, 0, 0, 0);*/
-    /*}*/
+
+
+
+
     .owl-carousel li {
         list-style:none;
         margin-right: .3em;
@@ -208,6 +211,7 @@
     .owl-carousel li img {
         border-radius:.5em;
         transition:transform .15s ease-out;
+        border: 1px orange solid;
     }
     .owl-carousel li img:hover {
         transform:scale(.98, .98);
@@ -237,197 +241,101 @@
         overflow: hidden;
     }
 
+    #about_us_words{
+        background-color:#323333;
+    }
+#wrappa2{
+    background-color:#323333;
+}
 
-    .center-block {
-        background-color:silver;
+    .article-texterson2 {
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
     }
 
+    .show-main-img {
+        border: 1px solid #ff9000;
+        max-width:100%  !important;
+        height:auto;
+        display:inline;
+    }
+
+
     .contact_header,.contact_header2{font-variant:small-caps;
-        font-size: 2em !important;
+        font-size: 3em !important;
         text-align:center;
-        color:#ff9000;
-        text-shadow: none !important;}
+        color:#fcb704;
+        font-family: CheckLogo, Tahoma, Arial, "Trebuchet MS";
+        text-shadow: 6px 1px 10px #bb2103 !important;
+        line-height: .9em;
+    }
 
     .subheader-main {
         text-align:center;
 
 
     }
+   .article-gallery-header-img {
+
+        text-align:center;
+    }
+
+
+    .facebook-comment-header {
+    color: #3c55a2;
+        text-align:center;
+    }
+
+    .facebook-section-comments {
+        background-color: #d3d8da;
+
+    }
+
+
+    .owl-theme .owl-controls .owl-buttons div {
+        color: #FFF;
+        /*display: inline;*/
+        zoom: 1;
+        margin: 5px;
+        padding: 3px 10px;
+        font-size: 12px;
+        -webkit-border-radius: 30px;
+        -moz-border-radius: 30px;
+        border-radius: 30px;
+        background: #ff9000;
+        /*filter: Alpha(Opacity=50);*/
+         opacity: 1.0;
+    }
+
+    .owl-stuff{
+        width:100%;
+        height:auto !important;
+
+
+    }
+
+
+
+    img.logo-show {
+
+        background-color: #1d1d1d !important;
+        height:auto !important;
+    }
+
+    /*.woodsy-pollute{*/
+        /*margin-left:auto;*/
+        /*margin-right:auto;*/
+    /*}*/
+
 </style>
 
 
-<script src="{{url('/js/jquery.js')}}"></script>
+{{--<script src="{{url('/js/jquery.js')}}"></script>--}}
 
 
 
-<script>
 
-    $(function(){
-
-        // Drawing the HTML for PhotoSwipe
-        function buildPswdHtml(){
-            $("body").append([
-                '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">',
-                '  <div class="pswp__bg"></div>',
-                '  <div class="pswp__scroll-wrap">',
-                '    <div class="pswp__container">',
-                '      <div class="pswp__item"></div>',
-                '      <div class="pswp__item"></div>',
-                '      <div class="pswp__item"></div>',
-                '    </div>',
-                '    <div class="pswp__ui pswp__ui--hidden">',
-                '      <div class="pswp__top-bar">',
-                '          <div class="pswp__counter"></div>',
-                '          <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>',
-                '          <button class="pswp__button pswp__button--share" title="Share"></button>',
-                '          <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>',
-                '          <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>',
-                '          <div class="pswp__preloader">',
-                '            <div class="pswp__preloader__icn">',
-                '              <div class="pswp__preloader__cut">',
-                '                <div class="pswp__preloader__donut"></div>',
-                '              </div>',
-                '            </div>',
-                '          </div>',
-                '      </div>',
-                '      <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">',
-                '        <div class="pswp__share-tooltip"></div> ',
-                '      </div>',
-                '      <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>',
-                '      <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>',
-                '      <div class="pswp__caption">',
-                '        <div class="pswp__caption__center"></div>',
-                '      </div>',
-                '    </div>',
-                '  </div>',
-                '</div>'
-            ].join(""));
-        }
-
-
-        // From the gallery, get the items for PhotoSwipe
-        function getGalleryItems($gallery){
-            var items = [];
-
-            $gallery.find("a").each(function(){
-                var $anchor = $(this),
-                        size = $anchor.attr("data-size").split("x"),
-                        title = $anchor.attr("data-title"),
-                        item = {
-                            el: $anchor.get(0),
-                            src: $anchor.attr("href"),
-                            w: parseInt(size[0]),
-                            h: parseInt(size[1])
-                        };
-
-                // caption
-                if( title ) item.title = title;
-
-                items.push(item);
-            });
-
-            return items;
-        }
-
-
-        //Opening the PhotoSwipe
-        function openGallery($gallery, index, items, pswpOptions){
-            var $pswp = $(".pswp"),
-                    owl = $gallery.data("owlCarousel"),
-                    gallery;
-
-            //Set an option value
-            var options = $.extend(true, {
-                // Image number to open
-                index: index,
-
-                //Zoom setting at the time of image click
-                getThumbBoundsFn: function(index){
-                    var $thumbnail = $(items[index].el).find("img"),
-                            offset = $thumbnail.offset();
-                    return {
-                        x: offset.left,
-                        y: offset.top,
-                        w: $thumbnail.outerWidth()
-                    };
-                }
-            }, pswpOptions);
-
-            //Display the PhotoSwipe
-            gallery = new PhotoSwipe($pswp.get(0), PhotoSwipeUI_Default, items, options);
-            gallery.init();
-
-            // In accordance with the switching of PhotoSwipe slide , OwlCarousel also adjusts position
-            gallery.listen("beforeChange", function(x){
-                owl.goTo(this.getCurrentIndex());
-            });
-
-            gallery.listen("close", function(){
-                this.currItem.initialLayout = options.getThumbBoundsFn(this.getCurrentIndex());
-            });
-        }
-
-
-        // Initialization
-        function initializeGallery($elem, owlOptions, pswpOptions){
-
-            //If the DOM for PhotoSwipe does not exist , a new drawing
-            if( $(".pswp").size() === 0 ){
-                buildPswdHtml();
-            }
-
-            // Scan to accommodate a plurality of gallery
-            $elem.each(function(i){
-                var $gallery = $(this),
-                        uid = i + 1,
-                        items = getGalleryItems($gallery),
-                        options = $.extend(true, {}, pswpOptions);
-
-                // Initialization of OwlCarousel
-                $gallery.owlCarousel(owlOptions);
-
-                //Assign a unique ID to each gallery
-                options.galleryUID = uid;
-                $gallery.attr("data-pswp-uid", uid);
-
-                // With the click of each item , start PhotoSwipe
-                $gallery.find(".owl-item").on("click", function(e){
-                    if( !$(e.target).is("img") ) return;
-
-                    //items pass a copy because it is rewritten to PhotoSwipe.init ()
-                    openGallery($gallery, $(this).index(), items.concat(), options);
-                    return false;
-                });
-            });
-        }
-
-
-        // In the sample to perform the processing for the `.owl-carousel`
-        var owlOptions = {
-            //what to mess with if you want to change the amount of slides on the page no matter what as default 3 will show up...
-//                    itemsCustom: [[0, 3]],
-                    items: 3,
-                    responsiveRefreshRate: 0,
-                    navigation: true,
-//                    pagination: true,
-//                    paginationNumbers: true,
-                    scrollPerPage: false,
-                    dots: true,
-                    dotsEach: true
-                },
-                pswpOptions = {
-                    bgOpacity: 0.9,
-                    history: false,
-                    shareEl: true
-                };
-
-        initializeGallery($(".owl-carousel"), owlOptions, pswpOptions);
-
-    });
-
-
-
-</script>
 
 
 
