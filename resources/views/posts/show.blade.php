109bf418356996fcdb6b8aab8ef6dd9af4413068
iplayguitar81 <iplayguitar81@yahoo.com>
2017-07-28 01:46:44 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -359,9 +359,9 @@
 <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 
 {{--<script src="{{url('/js/jquery-1.11.3.min.js')}}"></script>--}}
-<script src="{{url('/js/star-rating.js')}}"></script>
-<script src="{{url('/js/owl.carousel.js')}}"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script defer src="{{url('/js/star-rating.js')}}"></script>
+<script defer src="{{url('/js/owl.carousel.js')}}"></script>
+{{--<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>--}}
 
 <script src="{{url('/js/photoswipe.min.js')}}"></script>
 <script src="{{url('/js/photoswipe-ui-default.min.js')}}"></script>
